2c0e1c2607bf7ffeb0d929e765bc496821ba82ce
Ral <ralfael.queiroz@live.com>
2026-06-30 15:33:42 -0300

    split method

---
+++ extra/split.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    String s = "potato apple lemon";
+    String [] vect = s.split(" ");
+
+    System.out.println(vect[0]);
+    System.out.println(vect[1]);
+    System.out.println(vect[2]);
+
+        }
+}
